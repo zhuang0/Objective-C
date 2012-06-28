6fdca31a1a8be22c87956399dadca833dc9d6d66
zhuang0 <sheen.zhuang@gmail.com>
2012-06-28 11:48:21 +0800

    my pro1

---
+++ pro1/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  pro1
+//
+//  Created by zhuang hua on 12-6-28.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
---
+++ pro1/pro1AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  pro1AppDelegate.h
+//  pro1
+//
+//  Created by zhuang hua on 12-6-28.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface pro1AppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end

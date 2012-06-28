5cca45bdc6712c9d60a887a1e1a9613359c48646
zhuang0 <sheen.zhuang@gmail.com>
2012-06-28 18:20:22 +0800

    pro2 about virtual printer

---
+++ pro2/pro2AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  pro2AppDelegate.h
+//  pro2
+//
+//  Created by zhuang hua on 12-6-28.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface pro2AppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end

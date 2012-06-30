785353701b598906cfb290004a085cd9b94f577c
zhuang0 <sheen.zhuang@gmail.com>
2012-06-30 16:03:43 +0800

    hello_iphone

---
+++ HelloWorld/Classes/HelloWorldViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by zhuang hua on 12-6-30.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+
+}
+
+@end
+
